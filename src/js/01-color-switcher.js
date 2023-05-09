fe48bc9ddf365bbf5bc748da9051ff09a0e855fd
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-09 15:05:26 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+ "use strict";
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
 
+btnStartRef.addEventListener('click', onBtnStart);
+btnStopRef.addEventListener('click', onBtnStop);
+
+let bgChanger = null;
+
+function onBtnStop() {
+    btnStartRef.removeAttribute('disabled');
+    clearInterval(bgChanger)
+}
+
+function onBtnStart() { 
+    btnStartRef.setAttribute('disabled', true);
+    bgChanger = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
