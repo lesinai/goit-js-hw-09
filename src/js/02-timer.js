22fb73ab263364a3a7403324825d65a1c2f75485
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-09 15:30:38 +0200

    upd tr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,45 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+
+const btnStartref = document.querySelector('[data-start]');
+const spanDays = document.querySelector('[data-days]');
+const spanHours = document.querySelector('[data-hours]');
+const spanMinutes = document.querySelector('[data-minutes]');
+const spanSeconds = document.querySelector('[data-seconds]');
+
+btnStartref.addEventListener('click', onBtnStartClick, { once: true} );
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (new Date() > selectedDates[0]) { alert('Please choose a date in the future'); }
+        btnStartref.removeAttribute('disabled');
+        },
+};
+
+const datePicker = flatpickr("#datetime-picker", options);
+
+
+function onBtnStartClick() {
+    const timeDiference = datePicker.selectedDates[0] - Date.now();
+
+    const startInterval = setInterval(() => {
+     const { days, hours, minutes, seconds } = convertMs(datePicker.selectedDates[0] - Date.now());
+        spanDays.textContent = addLeadingZero(days);
+        spanHours.textContent = addLeadingZero(hours);
+        spanMinutes.textContent = addLeadingZero(minutes);
+        spanSeconds.textContent = addLeadingZero(seconds);
+    }, 1000);
+
+    setTimeout(() => { clearInterval(startInterval) }, timeDiference)
+}
+   
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -20,6 +59,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
